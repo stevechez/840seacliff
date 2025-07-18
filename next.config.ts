0002a72dd6a51fde7f853beebc3e2bd1561cfd4a
Steve <stevechez@gmail.com>
2025-07-18 12:57:26 -0700

    ...

--- next.config.ts
+++ next.config.ts
@@ -10,8 +10,6 @@ const nextConfig: NextConfig = {
 		],
 	},
 	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
 		ignoreDuringBuilds: true,
 	},
 };
